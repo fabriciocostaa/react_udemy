d114628e322d17590e1f831e17a8ed9ce4b8c9d7
JulianaMitani <mitani.juliana@gmail.com.com>
2026-06-07 21:19:02 -0300

    ajuste

---
+++ chronos-pomodoro/chronos-pomodoro/src/components/CountDown/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+ 
+export function Count() {
+    return (        
+        <div className={styles.count}> 00:00 </div>
+    ) 
+}
+
+
---
+++ chronos-pomodoro/chronos-pomodoro/src/components/Menu/index.tsx
@@ -0,0 +1,23 @@
+import styles from './styles.module.css'
+import { HouseIcon, HistoryIcon, Settings2Icon, SunIcon } from 'lucide-react';
+ 
+export function Menu() {
+    return (        
+        <nav className={styles.menu}> 
+            <a className={styles.menuLink} href='#'>
+                <HouseIcon/> 
+            </a>
+            <a className={styles.menuLink} href='#'>  
+                <HistoryIcon/>
+            </a>
+            <a className={styles.menuLink} href='#'>
+                <Settings2Icon/>
+            </a>
+            <a className={styles.menuLink} href='#'>
+                <SunIcon/>
+            </a>
+        </nav>
+    ) 
+}
+
+
